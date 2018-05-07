97d3dd65b1381782ab0459403d6f30fe94c671cd
Matheus Sousa <matheus.sousa@concrete.com.br>
2018-05-07 14:53:50 -0300

    added models

---
+++ moviesapp/Models/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  moviesapp
+//
+//  Created by matheus.v.sousa on 5/7/18.
+//  Copyright © 2018 Emerson Batista. All rights reserved.
+//
+
+import Foundation
+
+struct Movie : Codable {
+    let id : Int
+    let title: String
+    let poster_path: String
+    let release_date: String
+    let genre_ids: [Int]
+    let overview: String
+}
