243168081decf5eb5516109d9ae9f3c4ceecccbb
Matheus Sousa <matheus.sousa@concrete.com.br>
2018-05-16 13:44:57 -0300

    Fixes collectionview selection bug

--- moviesapp/Controller/ListMovies/ListMoviesDatasource.swift
+++ moviesapp/Controller/ListMovies/ListMoviesDatasource.swift
@@ -64,7 +64,7 @@ extension ListMoviesDatasource: UICollectionViewDataSource {
 
 extension ListMoviesDatasource: UICollectionViewDelegate {
     
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         delegate?.didSelectCell(with: self.listMovies[indexPath.row])
     }
     
