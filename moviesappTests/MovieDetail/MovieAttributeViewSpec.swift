eb2e02ed57aac919a029a49948b479b776f9f124
Matheus Sousa <matheus.sousa@concrete.com.br>
2018-05-16 16:32:16 -0300

    Adds unit test

--- moviesappTests/MovieDetail/MovieAttributeViewSpec.swift
+++ moviesappTests/MovieDetail/MovieAttributeViewSpec.swift
@@ -1,5 +1,5 @@
 //
-//  MovieDetailViewSpec.swift
+//  MovieAttributeView.swift
 //  moviesappTests
 //
 //  Created by matheus.v.sousa on 5/15/18.
@@ -10,35 +10,30 @@ import Foundation
 import Quick
 import Nimble
 import Nimble_Snapshots
-import SDWebImage
 
 @testable import moviesapp
 
-class MovieDetailViewSpec: QuickSpec {
+class MovieAttributeViewSpec: QuickSpec {
     override func spec() {
-        describe("MovieDetailView") {
+        describe("'MovieAttributeView'") {
             
-            var sut: MovieDetailView!
+            var sut: MovieAttributeView!
             var movie: Movie!
             beforeEach {
+                let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 40)
                 movie = Movie(id: 0, title: "Ironamn", poster_path: "path", release_date: "2000", genre_ids: [], overview: "great movie")
-                sut = MovieDetailView(frame: UIScreen.main.bounds, movie: movie)
+                sut = MovieAttributeView(frame: frame, name: movie.title!)
             }
             
             it("should have the expected look and feel") {
-                expect(sut) == snapshot("MovieDetailView")
+                expect(sut) == snapshot("MovieAttributeView")
             }
             
             context("when initialized") {
                 
-                it("should have set the movie") {
-                    expect(sut.movie).toNot(beNil())
+                it("should have set the correct movie name") {
+                    expect(sut.name).to(equal(movie.title!))
                 }
-                
-                it("should have loaded the image correctly") {
-                    expect(sut.movieImage.image).toEventuallyNot(beNil())
-                }
-                
             }
         }
     }
